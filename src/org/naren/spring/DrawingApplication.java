be812505d6ef62d6584bab9c306f61249180766a
nkondety <nkondety@gmail.com>
2016-05-14 17:27:30 -0500

    intitial commit

---
+++ src/org/naren/spring/DrawingApplication.java
@@ -0,0 +1,20 @@
+package org.naren.spring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//BeanFactory factory= new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle=(Triangle)context.getBean("triangle");
+		triangle.draw();
+		
+	}
+
+}
